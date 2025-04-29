6c2d4871b40438bdd98132fa9c840a35ecf0e81f
Re9iNee <attarzadeh76@gmail.com>
2025-04-29 14:48:19 +0330

    seeder file

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await clearDatabase();
+}
+
+async function clearDatabase() {
+  await prisma.services.deleteMany();
+  console.log(`🌱 Deleted all services`);
+  await prisma.cars.deleteMany();
+  console.log(`🌱 Deleted all cars`);
+  await prisma.users.deleteMany();
+  console.log(`🌱 Deleted all users`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
