a409291afd0f2b2fb2a040f86786111a54b925d5
Re9iNee <attarzadeh76@gmail.com>
2025-05-11 21:43:49 +0330

    fix production

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from "../generated/prisma";
 
-const prisma = new PrismaClient({
-  datasources: { db: { url: process.env.DATABASE_URL } },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
 
-async function gracefulShutdown() {
-  console.log("\nShutting down gracefully...");
-  await prisma.$disconnect();
-}
+// async function gracefulShutdown() {
+//   console.log("\nShutting down gracefully...");
+//   await prisma.$disconnect();
+// }
 
-process.on("SIGINT", gracefulShutdown); // Ctrl+C
-process.on("SIGTERM", gracefulShutdown); // Sent by cloud providers (AWS, etc.)
+// process.on("SIGINT", gracefulShutdown); // Ctrl+C
+// process.on("SIGTERM", gracefulShutdown); // Sent by cloud providers (AWS, etc.)
